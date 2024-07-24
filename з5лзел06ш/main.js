817ade1d4d1225aaf763d1c13e28d2f7b3f093d1
Xusomiddinov <komolzokirova@gmail.com>
2024-07-24 10:54:26 +0500

    Add files via upload

---
+++ з5лзел06ш/main.js
@@ -0,0 +1,15 @@
+let abzal = [12, 34, 56, 78, "miyeni gdasldkmsf-e", true, false, 456, 5678]
+
+
+
+let a = prompt("xoxlagan narsangni kirit")
+
+
+let bosh = []
+
+
+bosh.push(a)
+
+console.log(bosh);
+
+
